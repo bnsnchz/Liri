5505167454d3a30bea86c2713ca759a7de385507
bnsnchz <bsanchez4488@gmail.com>
2018-06-09 10:42:03 -0700

    pushing finished twitter

--- liri.js
+++ liri.js
@@ -12,6 +12,23 @@ var command = process.argv[2];
 if (command==="my-tweets"){
     // client.GET statuses/user_timeline;
     console.log("this is a tweet");
+    var param = {screen_name: "BBNG_", 
+                 count:20,
+                 trim_user:true,
+                 include_entities:false,
+                 }
+    client.get("statuses/user_timeline", param, function(error, tweets) {
+      if (!error) {
+        for(var i = 0; i<tweets.length; i++){
+            var tweetText = tweets[i].text;
+            var tweetCreated = tweets[i].created_at;
+            console.log("----------------------------------");
+            console.log("Tweet: "+ tweetText);
+            console.log("Date: "+ tweetCreated);
+            console.log("----------------------------------");
+        }
+      }
+    });
 }
 
 
